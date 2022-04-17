c9b9b0c071785018b390c855cf9c78e9217514b4
DOJ <omarjeng999@gmail.com>
2022-04-17 21:36:12 +0000

    aimated text circle restructure

--- 03-AnimatedCircleText/app.js
+++ 03-AnimatedCircleText/app.js
@@ -2,6 +2,6 @@ const text = document.querySelector(".text p");
 text.innerHTML = text.innerHTML
   .split("")
   .map(
-    (char, i) => `<span style="transform:rotate(${i * 5}deg)">${char}</span>`
+    (char, i) => `<span style="transform:rotate(${i * 6}deg)">${char}</span>`
   )
   .join("");
