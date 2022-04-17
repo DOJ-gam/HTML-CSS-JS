b6e0f992da6f9ea510fee2104417cea310ba4318
DOJ <omarjeng999@gmail.com>
2022-04-17 21:51:28 +0000

    finish animatedCircleText

--- 03-AnimatedCircleText/app.js
+++ 03-AnimatedCircleText/app.js
@@ -2,6 +2,6 @@ const text = document.querySelector(".text p");
 text.innerHTML = text.innerHTML
   .split("")
   .map(
-    (char, i) => `<span style="transform:rotate(${i * 6.2}deg)">${char}</span>`
+    (char, i) => `<span style="transform:rotate(${i * 5.7}deg)">${char}</span>`
   )
   .join("");
