35dca32b7d09317a8e41797fbc4cd0c09fd9f318
DOJ <omarjeng999@gmail.com>
2022-04-18 22:26:36 +0000

    video player add close modal functionality

--- 02-VideoPopUpPlayer/app.js
+++ 02-VideoPopUpPlayer/app.js
@@ -1,7 +1,13 @@
 let btn = document.querySelector(".btn");
 let clip = document.querySelector(".clip");
+let close = document.querySelector(".close");
 
 btn.addEventListener("click", () => {
   btn.classList.add("active");
   clip.classList.add("active");
+});
+
+close.addEventListener("click", () => {
+  btn.classList.remove("active");
+  clip.classList.remove("active");
 });
