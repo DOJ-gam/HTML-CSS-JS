b9f901c3028147a96ff741e4e48238a2840fd87a
DOJ <omarjeng999@gmail.com>
2022-04-18 22:31:01 +0000

    video player finished

--- 02-VideoPopUpPlayer/app.js
+++ 02-VideoPopUpPlayer/app.js
@@ -1,13 +1,17 @@
 let btn = document.querySelector(".btn");
 let clip = document.querySelector(".clip");
 let close = document.querySelector(".close");
+let video = document.querySelector("video");
 
 btn.addEventListener("click", () => {
   btn.classList.add("active");
   clip.classList.add("active");
+  video.play();
+  video.currentTime = 0;
 });
 
 close.addEventListener("click", () => {
   btn.classList.remove("active");
   clip.classList.remove("active");
+  video.pause();
 });
