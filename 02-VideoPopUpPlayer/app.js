f2197557b90a1286ac1c46fa27c915c896bae737
DOJ <omarjeng999@gmail.com>
2022-04-18 21:35:27 +0000

    video player add active classes andd play video on click

--- 02-VideoPopUpPlayer/app.js
+++ 02-VideoPopUpPlayer/app.js
@@ -1,5 +1,7 @@
 let btn = document.querySelector(".btn");
+let clip = document.querySelector(".clip");
 
 btn.addEventListener("click", () => {
   btn.classList.add("active");
+  clip.classList.add("active");
 });
