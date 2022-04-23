3230481f0d4aaaa03ae3e5d06efdd58648d8f606
DOJ <omarjeng999@gmail.com>
2022-04-23 18:52:15 +0000

    Draggable Floating Menu add draggable with js

--- 04-DraggableFloatingActionMenu/app.js
+++ 04-DraggableFloatingActionMenu/app.js
@@ -5,3 +5,25 @@ toggle.addEventListener("click", (event) => {
   toggle.classList.toggle("active");
   navigation.classList.toggle("active");
 });
+
+function onDrag({ movementX, movementY }) {
+  let getStyle = window.getComputedStyle(navigation);
+  let leftVal = parseInt(getStyle.left);
+  let topVal = parseInt(getStyle.top);
+  navigation.style.left = `${leftVal + movementX}px`;
+  navigation.style.top = `${topVal + movementY}px`;
+  //   console.log("X", movementX);
+  //   console.log("Y", movementY);
+}
+
+navigation.addEventListener("mousedown", () => {
+  navigation.addEventListener("mousemove", onDrag);
+});
+
+// navigation.addEventListener("mouseover", () => {
+//   navigation.addEventListener("mousemove", onDrag);
+// });
+
+document.addEventListener("mouseup", () => {
+  navigation.removeEventListener("mousemove", onDrag);
+});
