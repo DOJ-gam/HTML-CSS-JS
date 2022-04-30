5738110c5f66ce262370fc25ecd28402211b239a
DOJ <omarjeng999@gmail.com>
2022-04-30 23:00:11 +0000

    AnimateOnMouseMove js FINISH

--- 05-AnimateOnMouseMove/app.js
+++ 05-AnimateOnMouseMove/app.js
@@ -6,6 +6,13 @@ document.addEventListener("mousemove", (e) => {
   heart.style.left = x + "px";
   heart.style.top = y + "px";
 
+  let size = Math.random() * 80;
+  heart.style.width = 20 + size + "px";
+  heart.style.height = 20 + size + "px";
+
+  let transformValue = Math.random() * 80;
+  heart.style.transform = "rotate(" + transformValue + "deg)";
+
   body.appendChild(heart);
 
   setTimeout(() => {
